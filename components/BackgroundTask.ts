2230e5019f3ca589d1bb65e75eb3c2ece4a0e33b
dongjin <gksgks666@naver.com>
2025-02-20 22:47:16 +0900

    백그라운드 작업 로직 수정

--- components/BackgroundTask.ts
+++ components/BackgroundTask.ts
@@ -2,35 +2,38 @@ import * as TaskManager from "expo-task-manager";
 import * as BackgroundFetch from "expo-background-fetch";
 import * as Notifications from "expo-notifications";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { dailyResetNotification } from "@/components/UpdateNotificationBar";
+import { updateNotificationBar } from "@/components/UpdateNotificationBar";
 
 const BACKGROUND_FETCH_TASK = "daily-reset-task";
 
+// 특정 시간 범위 확인 함수
+const isResetTime = () => {
+  const hours = new Date().getHours();
+  return hours >= 0 && hours < 9; // 00:00 ~ 08:59
+};
+
 TaskManager.defineTask(BACKGROUND_FETCH_TASK, async () => {
   try {
-    const today = new Date().toISOString().split("T")[0]; // YYYY-MM-DD 포맷
-    const lastReset = await AsyncStorage.getItem("lastResetDate");
+    if (!isResetTime()) return BackgroundFetch.BackgroundFetchResult.NoData;
 
-    if (lastReset !== today) {
-      const saved = await AsyncStorage.getItem("notifications");
-      if (saved) {
-        const notifications = JSON.parse(saved).map((e: any) => ({
-          ...e,
-          completed: false,
-        }));
+    const saved = await AsyncStorage.getItem("notifications");
 
-        await AsyncStorage.setItem(
-          "notifications",
-          JSON.stringify(notifications)
-        );
-        await AsyncStorage.setItem("lastResetDate", today); // 마지막 초기화 날짜 저장
-        await Notifications.dismissAllNotificationsAsync(); // 기존 알림 삭제
-        dailyResetNotification(notifications); // 알림 업데이트
-      }
+    if (saved) {
+      const notifications = JSON.parse(saved).map((e: any) => ({
+        ...e,
+        completed: false,
+      }));
+
+      await AsyncStorage.setItem(
+        "notifications",
+        JSON.stringify(notifications)
+      );
+      await Notifications.dismissAllNotificationsAsync(); // 기존 알림 삭제
+      await updateNotificationBar(notifications); // 알림 업데이트
     }
     return BackgroundFetch.BackgroundFetchResult.NewData;
   } catch (error) {
-    return BackgroundFetch.BackgroundFetchResult.Failed;
+    console.error("알림이 초기화되지 않음", error);
   }
 });
 
@@ -43,10 +46,9 @@ export const registerBackgroundTask = async () => {
     console.log("백그라운드 작업이 허용되지 않음");
     return;
   }
-
   await BackgroundFetch.registerTaskAsync(BACKGROUND_FETCH_TASK, {
-    minimumInterval: 60 * 60 * 24, // 24시간마다 실행 (실제 00시에 실행)
-    //stopOnTerminate: false, // 앱 종료 후에도 유지
-    //startOnBoot: true, // 기기 부팅 후에도 실행
+    minimumInterval: 60 * 60 * 9, // 9시간마다 실행
+    stopOnTerminate: false, // 앱 종료 후에도 유지
+    startOnBoot: true, // 기기 부팅 후에도 실행
   });
 };
